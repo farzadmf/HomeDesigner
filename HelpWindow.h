2d9557691f24d3fb67eb9a0b5f68522464c68857
Farzad Majidfayyaz <farzadmf@hotmail.com>
2016-04-04 18:46:15 -0400

    Finish the help window

--- HelpWindow.h
+++ HelpWindow.h
@@ -2,15 +2,18 @@
 
 #include <QMainWindow>
 #include <QLayout>
+#include <vector>
 
 class HelpWindow : public QMainWindow
 {
     QWidget* centralWidget;
     QGridLayout* layout;
+//    std::map<int, QVBoxLayout*> columnLayouts;
+    std::vector<QVBoxLayout*> columnLayouts;
 
     int verticalSpacing = 25;
 
-    void AddToColumn(QGridLayout* gridLayout, int column, int& row) const;
+    void AddToColumn(QGridLayout* gridLayout, int column);
 public:
     HelpWindow();
     ~HelpWindow();
